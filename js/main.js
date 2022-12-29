43a97d97d61bfccdca50882fccc18e590c12e116
ethanmartin223 <ethan_l_martin@etownstudents.org>
2022-12-28 23:36:49 -0500

    Improved animations and js; Changed the menu location and spacing on many elements

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("corner-logo").classList.remove('fade-in');
+  document.getElementById("corner-logo").classList.toggle('fade-out');
+
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.marginLeft = "25px";
+  document.getElementById("corner-logo").classList.toggle('fade-in');
+  document.getElementById("corner-logo").classList.toggle('fade-out');
 }
